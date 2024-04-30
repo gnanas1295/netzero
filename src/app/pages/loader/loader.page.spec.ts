72972d5304ed9f8b167e9809d48a3e29a56867a8
Gnanasekar Mani <gnanas1295@gmail.com>
2024-04-30 11:10:27 +0100

    routing from loader to login page

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
 import { LoaderPage } from './loader.page';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations : [LoaderPage],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoaderPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router= TestBed.get(Router);
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+  it('should go to login page after load', fakeAsync(() => {
+    spyOn(router, 'navigate')
+    component.ngOnInit();
+    
+    tick(1500);
+    expect(router.navigate).toHaveBeenCalledWith(['login'])
+  }))
 });
