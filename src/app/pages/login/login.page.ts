125285efad295d325e39f01c7678f1272ece5b7d
Gnanasekar Mani <gnanas1295@gmail.com>
2024-04-30 15:57:53 +0100

    changes in the login pages, icons and name

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginPageForm } from './login.page.form';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  form: FormGroup = new FormGroup({});
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = new LoginPageForm(this.formBuilder).createForm();
+
   }
 
   login(){
